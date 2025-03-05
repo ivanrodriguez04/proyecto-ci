ff3989e6c9fa4cdd120fbe28f03958d76f7f5d81
ivanrodriguez04 <ivan.rodriguez04@usa.edu.co>
2025-03-05 15:10:25 +0000

    camhio version 2bien todo6

--- funciones.py
+++ funciones.py
@@ -1,8 +1,9 @@
 def es_primo(x):
-    
+    if x < 2:
+        return False
     for n in range (2,x):
         if x % n == 0:
             return False
             
             
-        return True
+    return True
